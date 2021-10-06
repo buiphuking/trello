91bb23dc394eecf618a8ecad3d0e48aee7420422
buiphuking <buiphuking@gmail.com>
2021-10-06 15:30:07 +0700

    Design trello Front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,159 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <p>Hello</p>
-      </header>
+    <div className="trello-trungquandev-master">
+      <div className="navbar app">App Bar</div>
+      <div className="navbar board">Board Bar</div>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="http://vuotlen.com/img/king.jpg"
+                alt="trungquandev-alt-img"
+              />{" "}
+              Title: TRUNQUANDEV
+            </li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+          </ul>
+          <footer>Another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="http://vuotlen.com/img/king.jpg"
+                alt="trungquandev-alt-img"
+              />{" "}
+              Title: TRUNQUANDEV
+            </li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+          </ul>
+          <footer>Another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="http://vuotlen.com/img/king.jpg"
+                alt="trungquandev-alt-img"
+              />{" "}
+              Title: TRUNQUANDEV
+            </li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+          </ul>
+          <footer>Another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="http://vuotlen.com/img/king.jpg"
+                alt="trungquandev-alt-img"
+              />{" "}
+              Title: TRUNQUANDEV
+            </li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+          </ul>
+          <footer>Another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="http://vuotlen.com/img/king.jpg"
+                alt="trungquandev-alt-img"
+              />{" "}
+              Title: TRUNQUANDEV
+            </li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+          </ul>
+          <footer>Another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="http://vuotlen.com/img/king.jpg"
+                alt="trungquandev-alt-img"
+              />{" "}
+              Title: TRUNQUANDEV
+            </li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+          </ul>
+          <footer>Another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="http://vuotlen.com/img/king.jpg"
+                alt="trungquandev-alt-img"
+              />{" "}
+              Title: TRUNQUANDEV
+            </li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+            <li>What you'd like</li>
+          </ul>
+          <footer>Another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
