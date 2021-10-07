d378a05bd58f5f6584ca965410821a24415cfad6
buiphuking <buiphuking@gmail.com>
2021-10-07 23:17:48 +0700

    Clean React Architecture

---
+++ client/src/components/AppBar/AppBar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./AppBar.scss";
+function AppBar() {
+  return <nav className="navbar-app">App Bar</nav>;
+}
+export default AppBar;
---
+++ client/src/components/BoardBar/BoardBar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./BoardBar.scss";
+function BoardBar() {
+  return <nav className="navbar-board">Board Bar</nav>;
+}
+export default BoardBar;
